f3f7513302b85b9971bdd7e5e4e4e77bd5e27f7f
Vichayut Tubtim <6310682767@student.tu.ac.th>
2022-10-07 12:40:23 +0700

    Update settings.py

--- project1/settings.py
+++ project1/settings.py
@@ -127,5 +127,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CSRF_TRUSTED_ORIGINS = ["https://thawing-caverns-30797.herokuapp.com"]
 
-import django_heroku
-django_heroku.settings(locals())
+import os
+if '/app' in os.environ['HOME']:
+    import django_heroku
+    django_heroku.settings(locals())
